ff31409801a712dc13a88fb7ae40bce879da4b41
Vitaliy <vetal.com92@gmail.com>
2023-05-04 22:59:13 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13555;
+        int score = 20;
+        int result = price / score;
+        System.out.println(result);
+
+    }
+}
